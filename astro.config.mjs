8c49e09ff5335e7545a58520446e1dd4b3c35ded
Mikhail Remmele <mikhail.remmele@gmail.com>
2026-02-20 23:21:53 +0300

    add 404 and meta tags

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,13 @@
 import { defineConfig } from 'astro/config';
 
+const rawSite = process.env.SITE_URL ?? process.env.DOMAIN;
+const site = rawSite
+  ? rawSite.startsWith('http://') || rawSite.startsWith('https://')
+    ? rawSite
+    : `https://${rawSite}`
+  : undefined;
+
 export default defineConfig({
-  output: 'static'
+  output: 'static',
+  site
 });
