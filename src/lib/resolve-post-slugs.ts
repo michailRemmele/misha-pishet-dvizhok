a2f4da6a5aa05886b9a598f1f06c73db11dbcd7d
Mikhail Remmele <mikhail.remmele@gmail.com>
2026-02-21 17:43:32 +0300

    add view transitions and fix ci

--- src/lib/resolve-post-slugs.ts
+++ src/lib/resolve-post-slugs.ts
@@ -17,3 +17,7 @@ export function resolvePostDateSegment(post: PostEntry): string {
 export function resolvePostPath(post: PostEntry): string {
   return `/posts/${resolvePostDateSegment(post)}/${resolvePostSlug(post)}/`;
 }
+
+export function resolvePostTransitionName(post: PostEntry): string {
+  return `post-${resolvePostDateSegment(post)}-${resolvePostSlug(post)}`;
+}
