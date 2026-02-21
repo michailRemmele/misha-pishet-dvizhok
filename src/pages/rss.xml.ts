764fa222098ec5551d3fa036f074df8f0d3bec4b
Mikhail Remmele <mikhail.remmele@gmail.com>
2026-02-22 01:40:45 +0300

    add videos links format for rss

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -5,7 +5,30 @@ import sanitizeHtml from 'sanitize-html';
 import MarkdownIt from 'markdown-it';
 import { resolvePostPath } from '../lib/resolve-post-slugs';
 
-const parser = new MarkdownIt();
+const parser = new MarkdownIt({ html: true });
+
+const VIDEO_SRC_REGEX = /<(?:video|source)\b[^>]*\bsrc=(["'])(.*?)\1/i;
+const VIDEO_TAG_REGEX = /<video\b[\s\S]*?<\/video>/gi;
+const escapeHtml = (value: string): string =>
+  value
+    .replace(/&/g, '&amp;')
+    .replace(/"/g, '&quot;')
+    .replace(/</g, '&lt;')
+    .replace(/>/g, '&gt;');
+
+const replaceVideoEmbedsWithLinks = (html: string): string => {
+  return html.replace(VIDEO_TAG_REGEX, (videoHtml) => {
+    const srcMatch = videoHtml.match(VIDEO_SRC_REGEX);
+    const videoSrc = srcMatch?.[2];
+
+    if (!videoSrc) {
+      return '';
+    }
+
+    const escapedSrc = escapeHtml(videoSrc);
+    return `<p><a href="${escapedSrc}">Смотреть видео: ${escapedSrc}</a></p>`;
+  });
+};
 
 export const GET: APIRoute = async (context) => {
   const posts = await getCollection('posts');
@@ -21,7 +44,7 @@ export const GET: APIRoute = async (context) => {
       pubDate: post.data.date,
       link: resolvePostPath(post),
       content: post.body
-        ? sanitizeHtml(parser.render(post.body), {
+        ? sanitizeHtml(replaceVideoEmbedsWithLinks(parser.render(post.body)), {
             allowedTags: sanitizeHtml.defaults.allowedTags.concat(['img'])
           })
         : undefined
